cef5aed34ff383abfdf9bf9721bfdd17a129cb59
Ross Gray <rossagray@gmail.com>
2024-09-23 13:14:49 +0100

    Add theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 import daisyui from 'daisyui'
+import themes from 'daisyui/src/theming/themes'
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {}
   },
-  plugins: [daisyui]
+  plugins: [daisyui],
+  daisyui: {
+    themes: [
+      {
+        myTheme: {
+          ...themes['winter'],
+          primary: '#00bd7e'
+        }
+      }
+    ]
+  }
 }
